4a6bee745c9b4e86f9462bf14676150666693a75
Aravindanchellam <32244581+Aravindanchellam@users.noreply.github.com>
2024-01-13 18:11:09 +0530

    css changes

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('Width', window.innerWidth);
+
+function turnOn() {
+    document.getElementById('myImage').src=`https://www.w3schools.com/js/pic_bulbon.gif`
+}
+
+function turnOff() {
+    document.getElementById('myImage').src=`https://www.w3schools.com/js/pic_bulboff.gif`
+}
+
